34921f02b48640aefc9a596c7c7073b8c89c0737
adrian delgado zambrano <jodezamb@espol.edu.ec>
2022-01-12 02:41:01 -0500

    Creando el servidor

---
+++ libreria/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+def inicio(request):
+    return HttpResponse("<h1> Bienvenido a su Pagina De vuelos </h1>")
+# directamente a template haciendo una solicitud a traves dela url  
+def nosotros(request):
+    return render(request,'paginas/nosotros.html')
